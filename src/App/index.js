3dd436b86a06b6e1237fec1d5eea18eb2d73c655
b-capra <caprabmc@gmail.com>
2022-12-07 18:57:05 -0500

    Add and style attribution

--- src/App/index.js
+++ src/App/index.js
@@ -7,6 +7,10 @@ function App() {
     <div className="app">
       <BalanceCard />
       <SpendingCard />
+      <div className='attribution'>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/b-capra/expense-chart">Brett Capra</a>.
+      </div>
     </div>
   );
 }
