29a84f0f1f4fd22b3576045e2cb87e808cec2f74
b-capra <caprabmc@gmail.com>
2022-12-07 18:09:48 -0500

    Style top card

--- src/App/BalanceCard/index.js
+++ src/App/BalanceCard/index.js
@@ -1,9 +1,14 @@
 import './index.css';
+import logo from '../../logo.svg'
 
 const BalanceCard = () => {
   return (
     <div className='balance-card'>
-      My Balance
+      <div className='balance'>
+        <p className='text'>My balance</p>
+        <p className='amount'>$921.48</p>
+      </div>
+      <img src={logo} className='logo' />
     </div>
   )
 }
