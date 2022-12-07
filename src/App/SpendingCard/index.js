3e2e7cf55bce699f27e5eb976fc0f8436080a3d5
b-capra <caprabmc@gmail.com>
2022-12-07 16:50:55 -0500

    Refactor file structure

---
+++ src/App/SpendingCard/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+
+const SpendingCard = () => {
+
+}
+
+export default SpendingCard
---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+import BalanceCard from './BalanceCard';
+import SpendingCard from './SpendingCard';
+
+function App() {
+  return (
+    <div className="App">
+      <BalanceCard />
+      <SpendingCard />
+    </div>
+  );
+}
+
+export default App;
