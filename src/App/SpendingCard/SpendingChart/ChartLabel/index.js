646bb4e05c0585be280f93d6b46b5e6fda609f4a
b-capra <caprabmc@gmail.com>
2022-12-13 22:18:35 -0500

    Add labels to bars

---
+++ src/App/SpendingCard/SpendingChart/ChartLabel/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+const ChartLabel = (day) => {
+  return (
+    <div className="label">{day.day}</div>
+  )
+}
+
+export default ChartLabel
--- src/App/SpendingCard/SpendingChart/index.js
+++ src/App/SpendingCard/SpendingChart/index.js
@@ -1,14 +1,22 @@
 import './index.css'
 import ChartBar from "./ChartBar"
+import ChartLabel from './ChartLabel'
 
 const SpendingChart = (data) => {
-  const spending = data.data
+  const weeklySpend = data.data
 
   return (
-    <div className="chartContainer">
-      {spending.map(dailySpend => 
-        <ChartBar key={spending.indexOf(dailySpend)} amount={dailySpend.amount} />
-      )}
+    <div>
+      <div className="chartContainer">
+        {weeklySpend.map(dailySpend => 
+          <ChartBar key={weeklySpend.indexOf(dailySpend)} amount={dailySpend.amount} />
+        )}
+      </div>
+      <div className="labelContainer">
+        {weeklySpend.map(dailySpend =>
+          <ChartLabel key={weeklySpend.indexOf(dailySpend)} day={dailySpend.day} />  
+        )}
+      </div>
     </div>
   )
 }
